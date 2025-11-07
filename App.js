61232e1fc4867a88404f1f610512ef2756831e68
KennethHernan <kdelacruz18@autonoma.edu.pe>
2025-11-07 09:38:01 -0500

    Update app.js

---
+++ App.js
@@ -0,0 +1,12 @@
+// App.js
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <Router />
+    </SafeAreaProvider>
+  );
+}
